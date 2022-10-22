74a910d01cc36d953e0d45e07f81374ffabd8a8b
ghdud119 <81297311+ghdud119@users.noreply.github.com>
2022-10-22 23:44:54 +0900

    멤버 리스트 정렬 버그 수정, 시작달 입력규칙 날짜 범위 예외처리

--- Calendar/MemberList.h
+++ Calendar/MemberList.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include<stdlib.h>
 #include <vector>
 #include <algorithm>
 #include <string>
@@ -30,7 +31,7 @@ public:
 	void SetWorkingCalender(string yearMonth) { this->workingMonth = yearMonth; }
 	string GetWorkingCalender() { return this->workingMonth; }
 
-	//º¤ÅÍ Æ÷ÀÎÅÍ ¸®ÅÏ vector->first = startingMonth, vector->second = ID
+	//ë²¡í„° í¬ì¸í„° ë¦¬í„´ vector->first = startingMonth, vector->second = ID
 	vector<UserInformation> *GetMemberList() { return &memberList; }
 
 	bool FileInput();
@@ -38,9 +39,10 @@ public:
 
 private:
 	void Sort();
-	static bool Compare(UserInformation member1, UserInformation member2) { return member1.ID < member2.ID; }
+	static bool Compare(UserInformation member1, UserInformation member2) { return member1.startingMonth < member2.startingMonth; }
 
 private:
 	vector<UserInformation> memberList;
 	string workingMonth = "-1";
 };
+
